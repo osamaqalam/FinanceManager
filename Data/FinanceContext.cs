b45919736b5133ecc4c6084edc151bafa20da3dd
Osama <osama.qalam@hotmail.com>
2025-02-05 00:52:14 -0500

    Added main page using Blazor

---
+++ Data/FinanceContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using PersonalFinanceManager.Models;
+
+namespace FinanceManager.Data
+{
+    public class FinanceContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Budget> Budgets { get; set; }
+
+        private readonly IConfiguration _configuration;
+
+        public FinanceContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = _configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
